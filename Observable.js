7c8f3c743b781563988c0e4291f6515b0df41e3d
Solórzano <wendy_liz15@hotmail.com>
2019-05-22 13:49:07 -0600

    subscribe on click event

--- Observable.js
+++ Observable.js
@@ -1,65 +1,59 @@
 class Observable{
-    constructor(subscribe){
-      this._subscribe = subscribe;
-    }
-    subscribe(observer){
-      return this._subscribe(observer);    
-    }
-    
-    static timeout(time){
-      return new Observable(function subscribe(observer){
-        const handle = setTimeout(function(){
-          observer.next();
-          observer.complete();
-        },time);
-        
-        return {
-          unsubscribe(){
-            clearTimeout(handle);
-          }
-        };
-      });
-    }
-    
-    static fromEvent(dom,eventName){ 
-      return new Observable(function subscribe(observer){
-        
-        const handler = (ev)=>{
-        observer.next(ev)
-         };
-        
-        dom.addEventListener(eventName,handler);
-
-        return {
-          unsubscribe(){
-            dom.removeEventListener(eventName,handler)
-          }
-        }
-      });
-    }
+  constructor(subscribe){
+    this._subscribe = subscribe;
+  }
+  subscribe(observer){
+    return this._subscribe(observer);    
   }
   
-  debugger;
-  
-   const obs = Observable.timeout(500);
-    obs.subscribe({
-        next(v){
-            console.log("next");
-        },
-        complete(){
-            console.log("Done")
+  static timeout(time){
+    return new Observable(function subscribe(observer){
+      const handle = setTimeout(function(){
+        observer.next();
+        observer.complete();
+      },time);
+      
+      return {
+        unsubscribe(){
+          clearTimeout(handle);
         }
+      };
     });
-    
-   obs.subscribe({
-        next(v){
-            console.log("next");
-        },
-        complete(){
-            console.log("Done")
+  }
+  
+  static fromEvent(dom,eventName){ 
+    return new Observable(function subscribe(observer){
+      
+      const handler = (ev)=>{
+      observer.next(ev)
+       };
+      
+      dom.addEventListener(eventName,handler);
+
+      return {
+        unsubscribe(){
+          dom.removeEventListener(eventName,handler)
         }
+      }
     });
+  }
+}
+
+const button = document.getElementById("button");
+const clicks = Observable.fromEvent(button,"click");
+
+
+  clicks.subscribe({
+      next(v){
+          console.log("next");
+      },
+      complete(){
+          console.log("Done")
+      }
+  });
+  
+
+
+
   
   
-    
-    
